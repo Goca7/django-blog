345bb802e7265485c281fd71859edb6456239768
Gordana Perencevic <0i1yjael@students.codeinstitute.net>
2024-09-12 14:57:02 +0000

    Updated the Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-goca7-djangoblog-02u1cahhhjp.ws.codeinstitute-ide.net',
